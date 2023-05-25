5b240438b82ba2a45e3f2f6b147e36d8e122f8e7
PIYUJIN <yjyj020201@gmail.com>
2023-05-25 17:39:17 +0900

    change exception

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,11 @@
+import java.lang.Exception
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 import kotlin.collections.ArrayList
 
 fun main(){
-    var ex = Exercise()
+    val ex = Exercise()
 //    println(ex.now)
     ex.running()
 }
@@ -16,7 +18,7 @@ class Exercise {
     var memoList = ArrayList<Memo>()
     var dateList = mutableSetOf<LocalDate>()
 
-    // 초기 상태를 동물 정보를 입력받는 상태로 설정한다.
+    // 초기 상태를 메뉴 정보를 입력받는 상태로 설정한다.
     var programState = ProgramState.PROGRAM_STATE_INPUT_MENU_NUMBER
 
     fun running() {
@@ -24,8 +26,29 @@ class Exercise {
             // 프로그램 상태에 따른 분기
             when (programState) {
                 ProgramState.PROGRAM_STATE_INPUT_MENU_NUMBER -> {
-                    inputMenuNumber()
-                    if(inputNumber==3) {
+                    try {
+                        inputMenuNumber()
+                    } catch(e: Exception) {
+                        scan.nextLine()
+                        println("잘못된 정보입니다. 숫자로 다시 입력해주세요.")
+//                        programState = ProgramState.PROGRAM_STATE_INPUT_MENU_NUMBER
+                        continue
+                    }
+
+                    if (inputNumber !in 1..3) {
+                        println("잘못된 번호입니다. 1,2,3번의 번호 중 입력해주세요.")
+                        continue
+                    }
+
+                    if (inputNumber == 1) {
+                        programState = ProgramState.PROGRAM_STATE_INPUT_EXERCISE_INFO
+                    } else if (inputNumber == 2) {
+                        programState = ProgramState.PROGRAM_STATE_PRINT_EXERCISE_INFO
+                    } else if (inputNumber == 3) {
+                        println("프로그램이 종료되었습니다.")
+                        break
+                    }
+                    else {
                         break
                     }
                 }
@@ -45,84 +68,71 @@ class Exercise {
     }
 
     fun inputMenuNumber() {
-        while (true) {
-            try {
+//            try {
+                println()
                 println("메뉴를 선택해주세요.")
                 println("1. 오늘의 운동 기록")
                 println("2. 날짜별 운동 기록 보기")
                 println("3. 종료")
                 print("번호 입력 : ")
                 inputNumber = scan.nextInt()
-            } catch (e: Exception) {
-                println("잘못 입력하였습니다. 다시 입력해주세요.")
-            }
-
-            if (inputNumber !in 1..3) {
-                println("잘못된 번호입니다. 1,2,3번의 번호 중 입력해주세요.")
-                continue
-            }
-
-            if (inputNumber == 1) {
-                programState = ProgramState.PROGRAM_STATE_INPUT_EXERCISE_INFO
-                break
-            } else if (inputNumber == 2) {
-                programState = ProgramState.PROGRAM_STATE_PRINT_EXERCISE_INFO
-                break
-            } else if (inputNumber == 3) {
-                break
-            }
-            else {
-                break
-            }
-        }
+//            } catch (e: Exception) {
+//                println("잘못 입력하였습니다. 다시 입력해주세요.")
+//                programState = ProgramState.PROGRAM_STATE_INPUT_MENU_NUMBER
+//            }
     }
 
     fun inputExerciseInfo() {
         var type = " "
         var num = 0L
         var setNum = 0L
+        var date = " "
+        var localDate : LocalDate = now
         try {
+            print("날짜 : ")
+            date = scan.next()
+            localDate = LocalDate.parse(date, DateTimeFormatter.ISO_DATE)
             print("운동 종류 : ")
             type = scan.next()
             print("횟수 : ")
             num = scan.nextLong()
             print("세트 : ")
             setNum = scan.nextLong()
-        } catch (e: java.lang.Exception) {
-            println("잘못된 정보입니다.")
-        }
-        memoList.add(Memo(now, type, num, setNum))
-        dateList.add(now)
-    }
 
-    fun printExerciseInfo() {
-        for (i in memoList) {
-            println()
-            println("운동 종류 : ${i.type}")
-            println("횟수 : ${i.num}")
-            println("세트 : ${i.set}")
+//        memoList.add(Memo(now, type, num, setNum))
+//        dateList.add(now)
+            memoList.add(Memo(localDate, type, num, setNum))
+            dateList.add(localDate)
+        } catch (e: java.lang.Exception) {
+            scan.nextLine()
+            println("잘못된 정보입니다.")
         }
     }
 
     fun selectDate() {
         var sortedDateList = dateList.sorted()
         var n = 1
-        for (i in sortedDateList)
+        println()
+        for (i in sortedDateList) {
             println("$n. $i")
+            n++
+        }
 
             println()
-            println("날짜를 선택해주세요. (0.이전) : ")
+            print("날짜를 선택해주세요. (0.이전) : ")
             var inputNum = scan.nextInt()
             if (inputNum == 0) {
                 programState = ProgramState.PROGRAM_STATE_INPUT_MENU_NUMBER
+                return
             }
             if (inputNum > dateList.size) {
                 println("해당 번호가 없습니다. 다시 입력해주세요.")
+                return
             }
 
             for (i in memoList) {
                 var date = i.date
-                if(date==dateList.elementAt(inputNum-1)) {
+                if(date==sortedDateList.elementAt(inputNum-1)) {
                     println()
                     println("운동 종류 : ${i.type}")
                     println("횟수 : ${i.num}")
